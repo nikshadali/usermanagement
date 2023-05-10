832142d03d7f4d9e7bffcf3ad1346550d716e137
nikshad <nikali92100@gmail.com>
2023-05-10 16:38:15 +0500

    hash user password

--- server/controller/user.js
+++ server/controller/user.js
@@ -1,12 +1,18 @@
 import User from "../model/users.js";
-
+import bcrypt from "bcrypt";
 export const users = async (req, res) => {
   try {
     const isEmail = await User.findOne({ email: req.body.email });
 
     if (isEmail) return res.status(202).json("User already exist");
+    const salt = bcrypt.genSaltSync(10);
+    const password = bcrypt.hashSync(req.body.password, salt);
     console.log(req.body);
-    const user = new User(req.body);
+    const user = new User({
+      name: req.body.name,
+      email: req.body.email,
+      password: password,
+    });
     const userSave = await user.save();
     res.status(200).json(userSave);
   } catch (err) {
